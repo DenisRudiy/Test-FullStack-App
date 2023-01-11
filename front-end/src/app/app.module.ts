f51bd025ee93db388ea1cc4ffccb01d8a50bb57f
DenisRudiy <denryd2311@gmail.com>
2023-01-11 16:52:21 +0200

    init components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
+import { ListingPageComponent } from './listing-page/listing-page.component';
+import { MyListingPageComponent } from './my-listing-page/my-listing-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactPageComponent,
+    EditListingPageComponent,
+    ListingDetailPageComponent,
+    ListingDataFormComponent,
+    ListingPageComponent,
+    MyListingPageComponent,
+    NavBarComponent,
+    NewListingPageComponent
   ],
   imports: [
     BrowserModule,
