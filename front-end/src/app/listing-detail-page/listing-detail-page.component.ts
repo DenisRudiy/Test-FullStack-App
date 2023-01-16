f2f900abff313a7116719517b8a919db3ef9a1a6
DenisRudiy <denryd2311@gmail.com>
2023-01-16 21:13:13 +0200

    connect detail page with back-end

--- front-end/src/app/listing-detail-page/listing-detail-page.component.ts
+++ front-end/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { fakeListings } from '../fake-data'; 
+import { ListingsService } from '../listings.service';
 import { Listing } from '../types';
+
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -9,14 +10,22 @@ import { Listing } from '../types';
   styleUrls: ['./listing-detail-page.component.scss']
 })
 export class ListingDetailPageComponent implements OnInit{
+  isLoading: boolean = true;
   listing: Listing;
 
   constructor (
     private route: ActivatedRoute,
+    private listingsService: ListingsService,
   ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeListings.find(listing => listing.id === id)!;
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.listingsService.getListingById(id)
+    .subscribe(listing => {
+      this.listing = listing;
+      this.isLoading = false;
+    });
+    this.listingsService.addViewToListing(id)
+    .subscribe(() => console.log('Views updated!'));
   }
 }
