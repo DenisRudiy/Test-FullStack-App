1a901d142eb786a2162861d93c8f2ce19ae27d19
DenisRudiy <denryd2311@gmail.com>
2023-01-17 17:19:42 +0200

    add final function

--- front-end/src/app/edit-listing-page/edit-listing-page.component.ts
+++ front-end/src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ListingsService } from '../listings.service';
 import { Listing } from '../types';
-import { fakeMyListings } from '../fake-data';
 
 @Component({
   selector: 'app-edit-listing-page',
@@ -14,15 +14,19 @@ export class EditListingPageComponent implements OnInit{
   constructor (
     private route: ActivatedRoute,
     private router: Router,
+    private ListingsService: ListingsService,
   ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeMyListings.find(listing => listing.id === id)!;
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.ListingsService.getListingById(id)
+      .subscribe(listing => this.listing = listing);
   }
 
-  onSubmit(): void{
-    alert(`Saving changes to the listing...`)
-    this.router.navigateByUrl('/my-listings')
+  onSubmit({ name, description, price } : { name: any, description: any, price: any }): void {
+    this.ListingsService.editListing(this.listing.id, name, description, price)
+      .subscribe(() => {
+        this.router.navigateByUrl('/my-listings');
+      });
   }
 }
